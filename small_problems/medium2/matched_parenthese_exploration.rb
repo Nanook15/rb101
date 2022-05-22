41eea7a75304d20803dc42e2927e81b4bf51d4ee
Nanook15 <nanook.eth@protonmail.com>
2022-05-22 07:50:52 +0000

    22.05.22

--- small_problems/medium2/matched_parenthese_exploration.rb
+++ small_problems/medium2/matched_parenthese_exploration.rb
@@ -1,73 +1,33 @@
 # write a method that returns true if there are matching sets of 
 # either (),{},[],'' or ""
 
-
-# def paired?(collection)
-#   collection = collection.join('')
-#   p collection
-
-#   case 
-#   when collection.include?('(')
-#     collection.match?(/\A(?<paren>\(.*\g<paren>*\))*\z/)
-#   when collection.include?('{')
-#     collection.match?(/\A(?<paren>\{.*\g<paren>*\})*\z/)
-#   when collection.include?('[')
-#     collection.match?(/\A(?<paren>\[.*\g<paren>*\])*\z/)
-#   when collection.include?("'")
-#     collection.match?(/\A(?<paren>\'.*\g<paren>*\')*\z/)
-#   when collection.include?("\"")
-#     collection.match?(/\A(?<paren>\".*\g<paren>*\")*\z/)
-#   else
-#     false
-#   end
-# end
-
-# def balanced?(string)
-#  collection = []
-
-#  string.chars.each do |bracket|
-#   case
-#   when bracket == '(' || bracket == ')'
-#     collection << bracket
-#   when bracket == '{' || bracket == '}'
-#     collection << bracket
-#   when bracket == '[' || bracket == ']'
-#     collection << bracket
-#   when bracket == "'" || bracket == "'"
-#     collection << bracket
-#   when bracket == "\"" || bracket == "\""
-#     collection << bracket
-#   end
-#  end
-
-#  paired?(collection)
-# end
+PAIRS = { '(' => ')', '{' => '}', '[' => ']', "'" => "'", "\"" => "\"" }
 
 
 def balanced?(string)
-  parens = 0
-  string.each_char do |char|
-    parens += 1 if char == '(' 
-    parens -= 1 if char == ')'
-    break if parens < 0
-  end
+  collection = []
 
-  parens.zero?
+    string.each_char do |opening|
+      string.each_char do |closing|
+        if PAIRS.key?(opening) && PAIRS.[opening][closing]
+          collection << opening
+          collection << closing
+        end
+      end
+    end
+
+  collection
 end
 
-
-
-
 p balanced?('()()()')
-p balanced?('{}{}{}')
-p balanced?('[][][]')
-p balanced?("''''''")
-p balanced?("\"\"\"\"")
-p balanced?(')(')
-p balanced?('}{')
-p balanced?('][')
-p balanced?("'")
-p balanced?("\"")
-p balanced?('(){}[]')
-p balanced?("\"\"'")
-
+# p balanced?('{}{}{}')
+# p balanced?('[][][]')
+# p balanced?("''''''")
+# p balanced?("\"\"\"\"")
+# p balanced?(')(')
+# p balanced?('}{')
+# p balanced?('][')
+# p balanced?("'")
+# p balanced?("\"")
+# p balanced?('(){}[]')
+# p balanced?("\"\"'")
