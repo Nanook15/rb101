2bc471cfa00f2166cf4673d114b9e805fa332f4a
Nanook15 <nanook.eth@protonmail.com>
2022-06-11 11:43:50 +0000

    Update 11.06.22

---
+++ small_problems/debugging/getting_things_done.rb
@@ -0,0 +1,33 @@
+# # why does this code create a SystemStackError
+
+# def move(n, from_array, to_array)
+#   return if n == 0
+#   to_array << from_array.shift
+#   move(n - 1, from_array, to_array)
+# end
+
+# # Example
+
+# todo = ['study', 'walk the dog', 'coffee with Tom']
+# done = ['apply sunscreen', 'go to the beach']
+
+# move(2, todo, done)
+
+# p todo # should be: ['coffee with Tom']
+# p done # should be: ['apply sunscreen', 'go to the beach', 'study', 'walk the dog']
+
+def move(n, from_array, to_array)
+  return if n == 0
+  to_array << from_array.shift
+  move(n - 1, from_array, to_array)
+end
+
+# Example
+
+todo = ['study']
+done = ['apply sunscreen', 'go to the beach']
+
+move(2, todo, done)
+
+p todo # should be: ['coffee with Tom']
+p done # should be: ['apply sunscreen', 'go to the beach', 'study', 'walk the dog']
